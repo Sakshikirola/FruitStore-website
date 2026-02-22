6b7a162d303a798fe5552d396032a37e053f92f4
Sakshi kirola <kirolasakshi@gmail.com>
2026-02-22 13:27:13 +0530

    Banner added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,43 @@
+import { section } from 'motion/react-client'
+import React from 'react'
+import BannerPng from "../assets/fruits-splash.png" 
+import {motion} from "framer-motion"
+import { FadeUp } from './animation'
+
+const Banner = () => {
+  return <section>
+    <div className='md:px-12 px-6 py-10 grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 '>
+      {/* Banner Image */}
+      <div className='flex justify-center items-center'>
+        <img src={BannerPng} alt="" className='w-75 md:max-w-100 h-full object-cover'/>
+      </div> 
+
+      {/* Banner Info */}
+      <div className='flex flex-col justify-center'>
+        <div className='text-center md:text-left space-y-4 lg:max-w-100'>
+            <motion.h1 
+             variants={FadeUp(0.5)}
+             initial="hidden"
+             whileInView="visible"
+             viewport={{once:true}} 
+             className='text-3xl lg:text-6xl font-bold uppercase'>Brand Info
+            </motion.h1>
+            <p>
+             At FruitStore, we’re redefining healthy living. We deliver fresh, juicy, and nutrient-packed fruits straight from farms to your home. No compromise on quality, no delay in freshness.
+             Whether you're starting your day strong or fueling your fitness journey, we make healthy eating simple, affordable, and exciting.
+             Freshness you can taste. Health you can feel.
+            </p>
+            <motion.div
+              variants={FadeUp(1.5)}  
+              initial="hidden"
+              animate="visible" 
+              className='flex justify-center md:justify-start'>
+              <button className='bg-[#de2c4d]'>Learn More</button>
+            </motion.div>  
+        </div>
+      </div>
+    </div>
+  </section>
+}
+
+export default Banner
